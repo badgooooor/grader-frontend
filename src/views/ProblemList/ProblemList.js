76733740ffab52a116a6c2f42daf9386d7af0334
Unknown <nedondev@gmail.com>
2018-06-21 12:11:34 +0700

    Change button and complete search bar
    + Change button layout
    + complete search bar inside

--- src/views/ProblemList/ProblemList.js
+++ src/views/ProblemList/ProblemList.js
@@ -17,69 +17,117 @@ class ProblemItemButton extends Component {
     render() {
         const difficulty = this.props.difficulty;
         return (
-            <Link to={{ pathname: '/problems/:id' }}>
-                <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                    <p>{this.props.id}</p>
-                    {this.props.name}
-                    <span className={difficulty === 'Easy' ? "badge badge-success" : (difficulty === 'Medium' ? "badge badge-warning" : "badge badge-danger")}>{difficulty}</span>
-                    <p>{this.props.passed}</p>
-                </button>
-            </Link>
+            <tr> 
+            <td>
+                <Link to={{ pathname: '/problems/:id' }}>
+                    <div>
+                        <p class="text-dark">{this.props.id}</p>
+                    </div>
+                </Link>        
+            </td>
+            <td>
+                <Link to={{ pathname: '/problems/:id' }}>
+                    <div className="text-center">
+                        <p class="text-dark">{this.props.name}</p>
+                    </div>
+                </Link>
+            </td>
+            <td className="text-center">
+                <Link to={{ pathname: '/problems/:id' }}>
+                    <div className="text-center">
+                    <span className={difficulty === 'Easy' ? "badge badge-success " : (difficulty === 'Medium' ? "badge badge-warning" : "badge badge-danger")}>{difficulty}</span>
+                    </div>  
+                </Link>
+            </td>
+            <td>
+                <Link to={{ pathname: '/problems/:id' }}>
+                    <div className="text-center">
+                        <p className="text-justify text-center text-dark">{this.props.passed}</p>
+                    </div>  
+                </Link>         
+            </td>                            
+            </tr>
         );
     }
 }
+class ProblemList extends Component {
 
-
-class SearchBar extends Component {
     state = {
         query: ''
     }
+
+    searchData = mockUpData;
+
+    currentProblem(){
+        this.searchData = [];
+        if(this.SearchBar.value === '') this.searchData = mockUpData;
+        else
+            for(let index = 0 ; index < mockUpData.length ; index++){
+                if(mockUpData[index].name.indexOf(this.SearchBar.value) >= 0
+                    || mockUpData[index].id.toString().indexOf(this.SearchBar.value) >= 0
+                    || mockUpData[index].difficulty.indexOf(this.SearchBar.value) >= 0){
+                    this.searchData.push(mockUpData[index]);
+                }
+            }
+        return (
+            this.searchData
+        );
+    }
+
     handleQuery = () => {
         console.log(this.state.query);
+        this.searchData = this.currentProblem();
     }
     handleInputChange = () => {
         this.setState({
             query: this.SearchBar.value
         })
+        this.searchData = this.currentProblem();
     }
-    render() {
-        return (
-            <div id="custom-search-input">
-                <div className="input-group col-md-12">
-                    <input ref={input => this.SearchBar = input} onChange={this.handleInputChange} type="text" className="search-query form-control" placeholder="Search" />
-                    <span className="input-group-btn">
-                        <button onClick={this.handleQuery} className="btn" type="button">
-                            <span className="cui-magnifying-glass" />
-                        </button>
-                    </span>
-                </div>
-            </div>
-        );
-    }
-}
 
-class ProblemList extends Component {
     render() {
-        const problemList = mockUpData;
         return (
             <div>
                 <div className="container-fluid bd-content" >
                     <div style={divStyleHeightLine} />
                     <h1 className="bd-title" id="content">Problem List</h1>
                     <div style={divStyleHeightLine} />
-                    <SearchBar />
+                    <div id="custom-search-input">
+                        <div className="input-group col-md-12">
+                            <input ref={input => this.SearchBar = input} onChange={this.handleInputChange} type="text" className="search-query form-control" placeholder="Search" />
+                            <span className="input-group-btn">
+                                <button onClick={this.handleQuery} className="btn" type="button">
+                                    <span className="cui-magnifying-glass" />
+                                </button>
+                            </span>
+                        </div>
+                    </div>
                     <div style={divStyleHeightLine} />
                     <div className="list-group">
-                        <button type="button" className="list-group-item list-group-item-action active disabled d-flex justify-content-between align-items-center">
-                            <h3>#</h3>
-                            <h3>Problem name</h3>
-                            <h3>Level</h3>
-                            <h3>Passed</h3>
-                        </button>
-                        <ProblemItemButton id="0" name="test" passed="100" difficulty="Easy" />
-                        {
-                            problemList.map((problem, i) => <ProblemItemButton id={i} name={problem.name} passed={problem.passed} difficulty={problem.difficulty} />)
-                        }
+                        <table className="table table-hover ">
+                            <thead className="thead-inverse">
+                                <tr>
+                                    <th>
+                                        #
+                                    </th>
+                                    <th className="text-center">
+                                        Problem name
+                                    </th>
+                                    <th className="text-center">
+                                        Level
+                                    </th>
+                                    <th className="text-center">
+                                        Passed
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.searchData.map((problem) => <ProblemItemButton id={problem.id} name={problem.name} passed={problem.passed} difficulty={problem.difficulty} />)
+                                }
+                                <div style={{height : 100 + 'px'}}/>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
 
